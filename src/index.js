d50b3551a5a08b5f3e3928152ade171b1288b67d
Kris S <kristina19960712@gmail.com>
2022-08-02 21:52:16 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return Array.isArray(matrix) ? matrix.reduce((sum, current, index) => ([...sum, ...(index + 1) % 2 === 0 ? current.sort((a, b) => (b-a)) : current]), []) : [] ;
 }
